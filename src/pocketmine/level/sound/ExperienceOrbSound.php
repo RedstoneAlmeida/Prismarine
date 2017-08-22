03d9fdf311779f5dfb47e1e5be831859761c6fa6
Encritary <mrgenga537@gmail.com>
2017-08-22 14:38:41 +0500

    Implement ExperienceOrbSound

---
+++ src/pocketmine/level/sound/ExperienceOrbSound.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ *
+ *  ____       _                          _
+ * |  _ \ _ __(_)___ _ __ ___   __ _ _ __(_)_ __   ___
+ * | |_) | '__| / __| '_ ` _ \ / _` | '__| | '_ \ / _ \
+ * |  __/| |  | \__ \ | | | | | (_| | |  | | | | |  __/
+ * |_|   |_|  |_|___/_| |_| |_|\__,_|_|  |_|_| |_|\___|
+ *
+ * Prismarine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author Prismarine Team
+ * @link   https://github.com/PrismarineMC/Prismarine
+ *
+ *
+ */
+
+namespace pocketmine\level\sound;
+
+use pocketmine\math\Vector3;
+use pocketmine\network\mcpe\protocol\LevelEventPacket;
+
+class ExperienceOrbSound extends GenericSound {
+	/**
+	 * ExperienceOrbSound constructor.
+	 *
+	 * @param Vector3 $pos
+	 * @param int     $pitch
+	 */
+	public function __construct(Vector3 $pos, $pitch = 0){
+		parent::__construct($pos, LevelEventPacket::EVENT_SOUND_ORB, $pitch);
+	}
+}
