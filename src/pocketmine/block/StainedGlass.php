8e6f220b351389427059ccbbbb14f25b140cc912
Encritary <mrgenga537@gmail.com>
2017-08-25 20:29:16 +0500

    Fix Stained Glass

--- src/pocketmine/block/StainedGlass.php
+++ src/pocketmine/block/StainedGlass.php
@@ -38,4 +38,26 @@ class StainedGlass extends Glass{
 		return ColorBlockMetaHelper::getColorFromMeta($this->meta) . " Stained Glass";
 	}
 
+	public function getName(): string{
+		static $names = [
+			0 => "White Stained Glass",
+			1 => "Orange Stained Glass",
+			2 => "Magenta Stained Glass",
+			3 => "Light Blue Stained Glass",
+			4 => "Yellow Stained Glass",
+			5 => "Lime Stained Glass",
+			6 => "Pink Stained Glass",
+			7 => "Gray Stained Glass",
+			8 => "Light Gray Stained Glass",
+			9 => "Cyan Stained Glass",
+			10 => "Purple Stained Glass",
+			11 => "Blue Stained Glass",
+			12 => "Brown Stained Glass",
+			13 => "Green Stained Glass",
+			14 => "Red Stained Glass",
+			15 => "Black Stained Glass",
+		];
+		return $names[$this->meta & 0x0f];
+	}
+
 }
--- src/pocketmine/block/StainedGlassPane.php
+++ src/pocketmine/block/StainedGlassPane.php
@@ -34,8 +34,25 @@ class StainedGlassPane extends GlassPane{
 		$this->meta = $meta;
 	}
 
-	public function getName() : string{
-		return ColorBlockMetaHelper::getColorFromMeta($this->meta) . " Stained Glass Pane";
+	public function getName(): string{
+		static $names = [
+			0 => "White Stained Glass Pane",
+			1 => "Orange Stained Glass Pane",
+			2 => "Magenta Stained Glass Pane",
+			3 => "Light Blue Stained Glass Pane",
+			4 => "Yellow Stained Glass Pane",
+			5 => "Lime Stained Glass Pane",
+			6 => "Pink Stained Glass Pane",
+			7 => "Gray Stained Glass Pane",
+			8 => "Light Gray Stained Glass Pane",
+			9 => "Cyan Stained Glass Pane",
+			10 => "Purple Stained Glass Pane",
+			11 => "Blue Stained Glass Pane",
+			12 => "Brown Stained Glass Pane",
+			13 => "Green Stained Glass Pane",
+			14 => "Red Stained Glass Pane",
+			15 => "Black Stained Glass Pane",
+		];
+		return $names[$this->meta & 0x0f];
 	}
-
 }
