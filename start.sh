698b6c83ead5a8d4354cb70532f0688f6b3292ab
Encritary <mrgenga537@gmail.com>
2017-09-07 17:26:10 +0500

    new start.sh

--- start.sh
+++ start.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
-
-# This is the start.sh file for Prismarine
-# Please input ./start.sh to start server
-
-# Variable define
 DIR="$(cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
-
-# Change Directory
 cd "$DIR"
 
-# Loop starting
-# Don't edit this if you don't know what this does!
-
+# Automatically restart the server
 DO_LOOP="no"
-
-##########################################
-# DO NOT EDIT ANYTHING BEYOND THIS LINE! #
-##########################################
 
 while getopts "p:f:l" OPTION 2> /dev/null; do
 	case ${OPTION} in
@@ -39,28 +26,23 @@ if [ "$PHP_BINARY" == "" ]; then
 	if [ -f ./bin/php7/bin/php ]; then
 		export PHPRC=""
 		PHP_BINARY="./bin/php7/bin/php"
-	elif [ -f /usr/bin/php7.0 ]; then
-		export PHPRC=""
-		PHP_BINARY="/usr/bin/php7.0"
-	elif type php 2>/dev/null; then
+	elif [[ ! -z $(type php) ]]; then
 		PHP_BINARY=$(type -p php)
 	else
-		echo "[ERROR] Couldn't find a working PHP binary, please use the installer."
+		echo "Couldn't find a working PHP7 binary, please use the installer."
 		exit 1
 	fi
 fi
 
 if [ "$POCKETMINE_FILE" == "" ]; then
-	if [ -f ./PocketMine-PR.phar ]; then
-		POCKETMINE_FILE="./PocketMine-PR.phar"
-	elif [ -f ./Prismarine*.phar ]; then
-	    	POCKETMINE_FILE="./Prismarine*.phar"
-	elif [ -f ./PocketMine-MP.phar ]; then
+	if [ -f ./PocketMine-MP.phar ]; then
 		POCKETMINE_FILE="./PocketMine-MP.phar"
+	elif [ -f ./Prismarine*.phar ]; then
+		POCKETMINE_FILE="./Prismarine*.phar"
 	elif [ -f ./src/pocketmine/PocketMine.php ]; then
 		POCKETMINE_FILE="./src/pocketmine/PocketMine.php"
 	else
-		echo "[ERROR] Couldn't find a valid Prismarine installation."
+		echo "Couldn't find a valid Prismarine installation"
 		exit 1
 	fi
 fi
@@ -70,13 +52,17 @@ LOOPS=0
 set +e
 while [ "$LOOPS" -eq 0 ] || [ "$DO_LOOP" == "yes" ]; do
 	if [ "$DO_LOOP" == "yes" ]; then
-		"$PHP_BINARY" $POCKETMINE_FILE $@
+		"$PHP_BINARY" "$POCKETMINE_FILE" $@
 	else
-		exec "$PHP_BINARY" $POCKETMINE_FILE $@
+		exec "$PHP_BINARY" "$POCKETMINE_FILE" $@
 	fi
-	((LOOPS++))
+	if [ "$DO_LOOP" == "yes" ]; then
+		if [ ${LOOPS} -gt 0 ]; then
+			echo "Restarted $LOOPS times"
+		fi 
+		echo "To escape the loop, press CTRL+C now. Otherwise, wait 2 seconds for the server to restart."
+		echo ""
+		sleep 2
+		((LOOPS++))
+	fi
 done
-
-if [ ${LOOPS} -gt 1 ]; then
-	echo "[INFO] Restarted $LOOPS times"
-fi
