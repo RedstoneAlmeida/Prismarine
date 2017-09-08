1b3574bbfa8c41b7e683f9605693e2601bb07463
Encritary <mrgenga537@gmail.com>
2017-09-08 17:19:16 +0500

    1.2.0.31

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -78,7 +78,7 @@ namespace pocketmine {
 	use pocketmine\wizard\SetupWizard;
 	use raklib\RakLib;
 
-	const VERSION = "1.2.0.25";
+	const VERSION = "1.2.0.31";
 	const API_VERSION = "3.0.0";
 	const CODENAME = "Pumpkin Pie";
 
